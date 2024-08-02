1db456591ea9a6a7c581e9c80d7e5cb5ebc0c492
Trevor Edris <trevor.edris@gmail.com>
2024-08-01 21:48:21 -0400

    [7] Add Username to configuration

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -3,5 +3,11 @@ package errors
 import "github.com/rotisserie/eris"
 
 var (
-	NotImplementedError = eris.New("function not implemented")
+	NotImplementedError  = eris.New("function not implemented")
+	DefaultUsernameError = eris.New("must not use default username")
+	InvalidUsernameError = eris.New("invalid username")
 )
+
+func NewInvalidUsernameWithReasonError(reason string) error {
+	return eris.Wrap(InvalidUsernameError, reason)
+}
